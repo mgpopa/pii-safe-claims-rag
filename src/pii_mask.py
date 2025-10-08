403d28eee3def76388b3a05ea11ff89f08feae98
mgpopa <popamihnea19@gmail.com>
2025-10-08 11:30:34 +0200

    Create pii_mask.py

---
+++ src/pii_mask.py
@@ -0,0 +1,5 @@
+LACEHOLDERS = {
+    "PERSON": "[PERSON]", "ORG": "[ORG]", "GPE": "[GPE]", "LOC": "[LOC]",
+    "NORP": "[GROUP]", "DATE": "[DATE]", "TIME": "[TIME]", "MONEY": "[MONEY]",
+    "CARDINAL": "[NUM]"
+}
